b1c711b065cc08271289d1ad790e8253c50517ba
marth98 <marth_mandl@gmx.at>
2020-11-08 17:52:13 +0100

    doc started

--- HelloWExpress/test/test.js
+++ HelloWExpress/test/test.js
@@ -7,16 +7,6 @@ let should = chai.should();
 var assertChai = chai.assert;
 
 chai.use(chaiHttp);
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.strictEqual([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-
-
 describe('/GET', () => {
   it('it should status 200 and return hello world', (done) => {
     chai.request(server)
@@ -30,5 +20,14 @@ describe('/GET', () => {
               //res.body.should.have.lengthOf(3);
           done();
         });
+  });
+});
+
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.strictEqual([1, 2, 3].indexOf(4), -1);
+    });
   });
 });
---
+++ Lab2/README.md
@@ -0,0 +1,9 @@
+Links:
+
+Link Azure Web Apps dev:
+https://mymmwebapp.azurewebsites.net/
+
+Link Azure Web Apps production:
+
+
+further information in doc.pdf
