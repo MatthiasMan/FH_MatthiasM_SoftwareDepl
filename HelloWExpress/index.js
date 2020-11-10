59d95519bf551dc7fe29db5a2ec4f6fed775d307
marth98 <marth_mandl@gmx.at>
2020-11-10 22:27:40 +0100

    no message

--- HelloWExpress/index.js
+++ HelloWExpress/index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');;
 })
 
-
-module.exports = app.listen(port, () => {
+//module.exports =
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
