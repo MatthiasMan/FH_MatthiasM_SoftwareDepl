8d59ce25660de410d3ff6151671e7b98e48260a2
marth98 <marth_mandl@gmx.at>
2020-11-10 22:22:43 +0100

    no message

--- HelloWExpress/index.js
+++ HelloWExpress/index.js
@@ -3,11 +3,14 @@ const appInsights = require('applicationinsights');
 appInsights.setup('afd42de7-5ea1-4a25-8ce6-c7f18f48a896').start();
 const express = require('express')
 const app = express()
-const port = 3000
-module.exports = app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+const port = process.env || 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');;
 })
+
+
+module.exports = app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
