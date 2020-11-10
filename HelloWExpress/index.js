428815821550eb8aa75a344b929beb4937a56813
marth98 <marth_mandl@gmx.at>
2020-11-11 00:27:23 +0100

    no message

--- HelloWExpress/index.js
+++ HelloWExpress/index.js
@@ -22,10 +22,9 @@ const appInsights = require('applicationinsights');
 appInsights.setup('8029cdba-03e6-42a2-ba5f-033bb7819b7d').start();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).json('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-//j
--- HelloWExpress/test/test.js
+++ HelloWExpress/test/test.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-//let server = require('../index');
-//let should = chai.should();
+let server = require('../index');
+let should = chai.should();
 
-//var assertChai = chai.assert;
+var assertChai = chai.assert;
 
-/*chai.use(chaiHttp);
+chai.use(chaiHttp);
 describe('/GET', () => {
   it('it should status 200 and return hello world', (done) => {
     chai.request(server)
         .get('/')
         .end((err, res) => {
-              //res.should.have.status(200);
+              res.should.have.status(200);
               //res.should.have.property('str')
               //assertChai.lengthOf(res.body, 1);
               res.body.should.eq('Hello World!');
@@ -21,7 +21,7 @@ describe('/GET', () => {
           done();
         });
   });
-});*/
+});
 
 
 describe('Array', function() {
