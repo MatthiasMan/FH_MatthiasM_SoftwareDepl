c4cebece9afbca492b9422ab70b0fe9492722846
marth98 <marth_mandl@gmx.at>
2020-11-11 00:57:22 +0100

    no message

--- HelloWExpress/index.js
+++ HelloWExpress/index.js
@@ -5,7 +5,7 @@ const appInsights = require('applicationinsights');
 appInsights.setup('afd42de7-5ea1-4a25-8ce6-c7f18f48a896').start();
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello World!')
+  res.status(200).json('Hello Worldd!')
 })
 
 module.exports = app.listen(port, () => {
